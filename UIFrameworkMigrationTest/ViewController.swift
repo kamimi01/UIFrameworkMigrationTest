4820f546c57b3a4c3fc37c3e10ef92c4a37c1540
kamimi01 <awakaru0122@gmail.com>
2021-12-29 17:26:37 +0900

    feat(UIKitでアプリを作成):

---
+++ UIFrameworkMigrationTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIFrameworkMigrationTest
+//
+//  Created by Mika Urakawa on 2021/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
