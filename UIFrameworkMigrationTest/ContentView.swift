1dc6803d28b701ac2ac6937d0eb093ee3b8c67cc
kamimi01 <awakaru0122@gmail.com>
2021-12-29 18:04:20 +0900

    feat: SwiftUIへ移行

---
+++ UIFrameworkMigrationTest/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  UIFrameworkMigrationTest
+//
+//  Created by Mika Urakawa on 2021/12/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UIFrameworkMigrationTest/UIFrameworkMigrationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIFrameworkMigrationTestApp.swift
+//  UIFrameworkMigrationTest
+//
+//  Created by Mika Urakawa on 2021/12/29.
+//
+
+import SwiftUI
+
+@main  // アプリのエントリ
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView() // 最初に表示される View
+        }
+    }
+}
